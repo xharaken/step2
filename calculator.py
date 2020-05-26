587b072ad955866504c5ebc2ac492dcb6fb88257
Kentaro Hara <haraken@google.com>
2020-05-26 10:11:22 +0900

    temp

---
+++ calculator.py
@@ -0,0 +1,18 @@
+while True:
+    print '> ',
+    line = raw_input()
+    answer = 0
+    i = 0
+    while i < len(line):
+        if line[i].isdigit():
+            number = 0
+            while i < len(line) and line[i].isdigit():
+                number = number * 10 + int(line[i])
+                i += 1
+            answer += number
+        elif line[i] == '+':
+            i += 1
+        else:
+            print 'Invalid character found: ' + line[i]
+            exit(1)
+    print "answer = %d\n" % answer
