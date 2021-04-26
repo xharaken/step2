3f36ab4690626deddeb7c2a7d4bea959dddff05a
Kentaro Hara <xharaken3@gmail.com>
2021-04-26 21:40:42 +0900

    Modified large.txt

--- anagram/score_checker.py
+++ anagram/score_checker.py
@@ -1,6 +1,11 @@
 import sys
 
-# SCORE of each character:
+# How to use:
+#
+# $ python3 score_checker.py your_answer_file
+#
+
+# SCORES of the characters:
 # ----------------------------------------
 # | 1 point  | a, e, h, i, n, o, r, s, t |
 # | 2 points | c, d, l, m, u             |
@@ -8,6 +13,8 @@ import sys
 # | 4 points | j, k, q, x, z             |
 # ----------------------------------------
 SCORES = [1, 3, 2, 2, 1, 3, 3, 1, 1, 4, 4, 2, 2, 1, 1, 3, 4, 1, 1, 1, 2, 3, 3, 4, 3, 4]
+
+WORDS_FILE = "words.txt"
 
 def calculate_score(word):
     score = 0
@@ -24,7 +31,7 @@ def read_words(word_file):
     return words
 
 def main(answer_file):
-    words = read_words('words.txt')
+    words = read_words(WORDS_FILE)
     answer_words = read_words(answer_file)
     score = 0
     for answer_word in answer_words:
