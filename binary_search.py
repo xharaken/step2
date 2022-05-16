6fab00dd29f6e4a51ba56d84afd3a11e941111c2
Kentaro Hara <xharaken3@gmail.com>
2022-05-16 16:02:20 +0900

    minor fix

--- binary_search.py
+++ binary_search.py
@@ -1,5 +1,3 @@
-#! /usr/bin/python3
-
 def quick_sort(array, left, right):
     pivot = array[int((left + right) / 2)]
     i = left
@@ -27,15 +25,14 @@ def sort(array):
 def binary_search(array, target):
     left = 0
     right = len(array) - 1
-    while left <= right:
+    while left <= right - 1:
         middle = int((left + right) / 2)
-        print(left, middle, right)
         if array[middle] == target:
             return True
         if target < array[middle]:
-            right = middle - 1
+            right = middle
         else:
-            left = middle + 1
+            left = middle
     return False
 
 
@@ -45,9 +42,6 @@ array = list(map(int, input().split()))
 
 # Sort the array
 sort(array)
-print(array)
-for i in range(len(array) - 1):
-    assert(array[i] <= array[i + 1])
 
 while True:
     print("Number:", end="")
